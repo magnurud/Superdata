4f837d4f8d18194d022d6a0082de6cfebdd9aadd
havakv <haavard.kvamme@gmail.com>
2015-03-24 21:48:17 +0100

    changed z-vector to array

--- part2/havard/poisson.cpp
+++ part2/havard/poisson.cpp
@@ -9,37 +9,21 @@
   revised, october 2001
 */
 
-//#include <stddef.h>
-//#include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
-//#include <memory.h>
-//#include <math.h>
 #include <cmath>
 #include <unistd.h>
 #include "matrix.hh"
 using namespace std;
 
-//typedef double Real;
-
-/* function prototypes */
-//double *createRealArray (int n);
-//Real **createReal2DArray (int m, int n);
-//void transpose (Real **bt, Real **b, int m);
 
 extern "C" {
-    //void fst_(Real *v, int *n, Real *w, int *nn);
-    //void fstinv_(Real *v, int *n, Real *w, int *nn);
     void fst_(double *v, int *n, double *w, int *nn);
     void fstinv_(double *v, int *n, double *w, int *nn);
 }
 
 
 int main(int argc, char **argv) {
-    //Real *diag, **b, **bt, *z;
-    //Real pi, h, umax;
-    //int i, j, n, m, nn;
-
     /* the total number of grid points in each spatial direction is (n+1) */
     /* the total number of degrees-of-freedom in each spatial direction is (n-1) */
     /* this version requires n to be a power of 2 */
@@ -75,12 +59,7 @@ int main(int argc, char **argv) {
     MatrixMPI<double, ColMajor> b(m, WorldComm);
     int cols = b.getCols();
 
-    //diag = createRealArray (m);
     vector<double> diag(m);
-    //b    = createReal2DArray (m,m);
-    //bt   = createReal2DArray (m,m);
-    //double* z = createRealArray (nn);
-    //vector<double> z(nn);
 
     double h = 1./(double)n;
     //pi   = 4.*atan(1.);
@@ -96,32 +75,20 @@ int main(int argc, char **argv) {
         }
     }
 
-    //-------------------------------
-    //b.transpose();
-    //MPI_Comm_free(&WorldComm);
-	//MPI_Finalize();
-
-    //return 0;
-    //-------------------------------
-
-
 #pragma omp parallel for schedule(static)
     for (int j = 0; j < cols; ++j) {
-        vector<double> z(nn);
-        fst_(b.colFront(j), &n, &z.front(), &nn);
+        double *z = new double[nn];
+        fst_(b.colFront(j), &n, z, &nn);
+        delete [] z;
     }
 
-    //if (rank == 0) {
-        //b.print();
-    //}
-    //transpose (bt,b,m);
     b.transpose();
-    //sleep(10);
 
 #pragma omp parallel for schedule(static)
     for (int i = 0; i < cols; ++i) {
-        vector<double> z(nn);
-        fstinv_(b.colFront(i), &n, &z.front(), &nn);
+        double *z = new double[nn];
+        fstinv_(b.colFront(i), &n, z, &nn);
+        delete [] z;
     }
 
     // Displacement vector
@@ -140,17 +107,18 @@ int main(int argc, char **argv) {
 
 #pragma omp parallel for schedule(static)
     for (int i = 0; i < cols; ++i) {
-        vector<double> z(nn);
-        fst_(b.colFront(i), &n, &z.front(), &nn);
+        double* z = new double[nn];
+        fst_(b.colFront(i), &n, z, &nn);
+        delete [] z;
     }
 
-    //transpose (b,bt,m);
     b.transpose();
 
 #pragma omp parallel for schedule(static)
     for (int j = 0; j < cols; j++) {
-        vector<double> z(nn);
-        fstinv_(b.colFront(j), &n, &z.front(), &nn);
+        double *z = new double[nn];
+        fstinv_(b.colFront(j), &n, z, &nn);
+        delete [] z;
     }
 
     double umax = 0.0;
