9ef25dd9c1d1378e81115b3786412044a301e87e
havakv <haavard.kvamme@gmail.com>
2015-03-27 09:43:38 +0100

    added max error

--- part2/havard/poisson.cpp
+++ part2/havard/poisson.cpp
@@ -23,17 +23,28 @@ extern "C" {
     void fstinv_(double *v, int *n, double *w, int *nn);
 }
 
+double sourceFunction(double x,double y);
+double solution(double x,double y);
+
 
 int main(int argc, char **argv) {
     /* the total number of grid points in each spatial direction is (n+1) */
     /* the total number of degrees-of-freedom in each spatial direction is (n-1) */
     /* this version requires n to be a power of 2 */
-
-    if (argc < 2) {
-        cout << "Need dim N" << endl;
+    if (argc < 3) {
+        cout << "Run poisson with: " << endl;
+        cout << "Arg 1:\t Problem size k.    rows = cols = 2^k" << endl << endl;
+        cout << "Arg 2: ";
+        cout << "\t 0: Run everything" << endl;
+        cout << "\t 1: Timing" << endl;
+        cout << "\t 2: Max error" << endl;
         exit(1);
     }
 
+    int n     = pow(2, atoi(argv[1]));
+    int m     = n-1;
+    int nn    = 4*n;
+    int prob  = atoi(argv[2]);
 
     int rank, size;
     MPI_Init(&argc, &argv);
@@ -46,33 +57,30 @@ int main(int argc, char **argv) {
     if (rank == 0)
         startTime = WallTime();
 
-    size_t N = atoi(argv[1]); // Dimensions of total matrix
-    if (N%2) {
-        cout << "Need N to not be odd" << endl;
-        MPI_Finalize();
-        return -1;
-    }
-
-    int n     = atoi(argv[1]);
-    int m     = n-1;
-    int nn    = 4*n;
-
     MatrixMPI<double, ColMajor> b(m, WorldComm);
     int cols = b.getCols();
 
+    // Eigenvalue vector
     vector<double> diag(m);
-
     double h = 1./(double)n;
-    //pi   = 4.*atan(1.);
 
+#pragma omp parallel for schedule(static)
     for (int i = 0; i < m; ++i) {
         diag[i] = 2.*(1.-cos((i+1)*M_PI/(double)n));
+    }
+
+    // Displacement vector
+    int displacement = 0;
+    vector<size_t> colsAll = b.getCols_all();
+    for (size_t i = 0; i < (size_t)rank; ++i) {
+        displacement += colsAll[i];
     }
 
 #pragma omp parallel for schedule(static)
     for (int j = 0; j < cols; ++j) {
         for (int i = 0; i < m; ++i) {
-            b(i,j) = h*h;
+            b(i,j) = h*h * 
+                sourceFunction( (j+displacement+1)*h, (i+1)*h);
         }
     }
 
@@ -90,13 +98,6 @@ int main(int argc, char **argv) {
         double *z = new double[nn];
         fstinv_(b.colFront(i), &n, z, &nn);
         delete [] z;
-    }
-
-    // Displacement vector
-    int displacement = 0;
-    vector<size_t> colsAll = b.getCols_all();
-    for (size_t i = 0; i < (size_t)rank; ++i) {
-        displacement += colsAll[i];
     }
     
 #pragma omp parallel for schedule(static)
@@ -122,78 +123,49 @@ int main(int argc, char **argv) {
         delete [] z;
     }
 
-    double umax = 0.0;
-    for (int j = 0; j < cols; j++) {
-        for (int i = 0; i < m; i++) {
-            //if (b[j][i] > umax) umax = b[j][i];
-            if (b(i, j) > umax) umax = b(i, j);
+    if (rank == 0 && (prob == 0 || prob == 1))
+        cout << "Time:\t" << WallTime() - startTime << endl;
+
+    // Check solution
+    if (prob == 0 || prob == 2) {
+        double maxErr = 0.0;
+        for (int i=0; i < m; i++) {
+            for (int j=0; j < cols; j++) {
+                if (fabs(b(i,j)-solution( (j+displacement+1)*h , (i+1)*h)) > maxErr) {
+                    maxErr = fabs(b(i,j)-solution( (j+displacement+1)*h , (i+1)*h));
+                }
+            }
         }
+        double totMaxErr;
+        MPI_Reduce (&maxErr, &totMaxErr, 1, MPI_DOUBLE, MPI_MAX, 0, WorldComm);
+        if (rank == 0) 
+            cout << "Maxerr:\t" << totMaxErr << endl;
     }
-    //printf (" umax = %e \n",umax);
-    cout << rank << ": umax = " << umax << endl;
-
-    if (rank == 0)
-        cout << "Time: " << WallTime() - startTime << endl;
-
-    //if (m == 9 && size == 3) {
-        //// Write to file
-        //MPI_File fh;
-        //char fileName[] = "poisson.txt";
-        //MPI_File_open(WorldComm, fileName, MPI_MODE_WRONLY|MPI_MODE_CREATE,
-                //MPI_INFO_NULL, &fh);
-        //MPI_Offset mysize = m*cols;
-        ////MPI_File_set_view(f, 0 , MPI_DOUBLE, filetype, "native", MPI_INFO_NULL);
-        //MPI_File_write_at(fh, rank*mysize*sizeof(double),
-                //b.colFront(0), m*cols, MPI_DOUBLE, MPI_STATUS_IGNORE);
-        //MPI_File_close(&fh);
-    //}
-
-    // Write to file
-    //for (int i = 0; i < size; ++i) {
-        //if (rank == i) {
-            //if (rank == 0) {
-                //ofstream out("poisson.txt");
-                //if (out.fail()) {
-                    //cout << "Problems with: poisson.txt" << endl;
-                    //exit(1);
-                //}
-                //cout << rank << " inside" << endl;
-                //for (int k=0; k < cols; k++) {
-                    //for (int j=0; j < m; j++) {
-                        //out << b(j,k) << "\t";
-                    //}
-                    //out << endl;
-                //}
-                //out.close();
-                //cout << rank << " done" << endl;
-            //} else {
-                //ofstream out("poisson.txt", fstream::app);
-                //if (out.fail()) {
-                    //cout << "Problems with: poisson.txt" << endl;
-                    //exit(1);
-                //}
-                //cout << rank << " inside" << endl;
-                //for (int k=0; k < cols; k++) {
-                    //for (int j=0; j < m; j++) {
-                        //out << b(j,k) << "\t";
-                    //}
-                    //out << endl;
-                //}
-                //out.close();
-                //cout << rank << " done" << endl;
-            //}
-            //sleep(2);
-            //cout << rank << endl;
-            //MPI_Barrier(WorldComm);
-        //}
-    //}
-
-
 
     MPI_Comm_free(&WorldComm);
     MPI_Finalize();
-
     return 0;
 }
 
 
+double sourceFunction(double x,double y){
+	double pi   = 4.*atan(1.);
+	/*return sin(2*pi*x)*sin(pi*y);*/
+	//
+	// For error testing START //
+	return 5*pi*pi*sin(pi*x)*sin(2*pi*y);
+	// For error testing END //
+	//
+	/*return exp(x)*sin(2*pi*x)*sin(pi*y);*/
+
+	// Point charge alternative START //
+	/*if(x== 0.5 && y ==0.75) return 1;*/
+	/*else if(x== 0.5 && y ==0.25) return -1;*/
+	/*else return 0;*/
+	// Point charge alternative END //
+}
+
+double solution(double x,double y){
+	double pi   = 4.*atan(1.);
+	return sin(pi*x)*sin(2*pi*y);
+}
